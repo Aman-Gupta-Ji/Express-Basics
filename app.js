07a9bfdae341ea03f8acd4e395f1918ab5806e4b
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-08-16 21:53:04 +0530

    routing .use

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+  console.log('This always runs');
+  next();
+})
 
-server.listen(3000);
+app.use("/add-product", (req, res, next) => {
+  console.log("In a middleware");
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);
