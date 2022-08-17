cc763b8e26540d7300af435f639101f144653aa8
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-08-17 17:16:39 +0530

    Filter Routes

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const adminRoutes = require("./routes/admin");
-const shopRoutes = require('./routes/shop');
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+app.use("/shops", shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>404 Page Not Found</h1>')
+  res.status(404).send("<h1>404 Page Not Found</h1>");
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-app.get("/admin/add-product", (req, res, next) => {
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post("/admin/add-product", (req, res) => {
+// /admin/add-product => POST
+router.post("/add-product", (req, res) => {
   // with .post only works for post requests
   console.log(req.body);
-  res.redirect("/");
+  res.redirect("/shops/");
 });
 
 module.exports = router;
