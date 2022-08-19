a150e1b0975b1c44288174c195c6b4c94d5f92ac
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-08-19 10:08:54 +0530

    path

--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,7 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
 });
 
 module.exports = router;
