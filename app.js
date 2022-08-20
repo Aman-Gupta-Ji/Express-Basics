c1d4dc8f82b3b672192b91971c80a340fec83965
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-08-20 07:51:42 +0530

    using pug & pug-layouts

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ const path = require("path");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use("/shops", shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "404", path: "/404" });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -5,16 +5,19 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
 });
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res) => {
   // with .post only works for post requests
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/shops/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
